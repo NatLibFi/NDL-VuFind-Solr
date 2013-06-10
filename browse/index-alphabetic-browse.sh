4e416023b493fca17cf5b17dc49603eecbde9aab
Ere Maijala <ere.maijala@helsinki.fi>
2013-06-10 13:23:22 +0300

    Added browse index creation class and script.

---
+++ browse/index-alphabetic-browse.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd "`dirname $0`"
+CLASSPATH="browse-indexing.jar:../solr-webapp/webapp/WEB-INF/lib/*"
+
+bib_index="../vufind/biblio/index"
+auth_index="../vufind/authority/index"
+index_dir="../vufind/alphabetical_browse"
+
+mkdir -p "$index_dir"
+
+function build_browse
+{
+    browse=$1
+    field=$2
+    skip_authority=$3
+
+    extra_jvm_opts=$4
+
+    if [ "$skip_authority" = "1" ]; then
+        java ${extra_jvm_opts} -Dfile.encoding="UTF-8" -Xmx1024m -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Dfield.preferred=heading -Dfield.insteadof=use_for -cp $CLASSPATH CreateBrowseSQLite "$bib_index" "$field" "${browse}_browse.db"
+    else
+        java ${extra_jvm_opts} -Dfile.encoding="UTF-8" -Xmx1024m -XX:+UseConcMarkSweepGC -XX:+CMSIncrementalMode -Dfield.preferred=heading -Dfield.insteadof=use_for -cp $CLASSPATH CreateBrowseSQLite "$bib_index" "$field" "$auth_index" "${browse}_browse.db"
+    fi
+
+    mv "${browse}_browse.db" "$index_dir/${browse}_browse.db-updated"
+    touch "$index_dir/${browse}_browse.db-ready"
+}
+#build_browse "hierarchy" "hierarchy_browse"
+build_browse "title" "title_fullStr" 1 "-Dbibleech=StoredFieldLeech -Dsortfield=title_sort -Dvaluefield=title_fullStr -Dfilterfield=building:source_str_mv"
+build_browse "topic" "topic_browse" 1 "-Dbibleech=StoredFieldLeech -Dsortfield=topic -Dvaluefield=topic -Dfilterfield=building:source_str_mv"
+build_browse "author" "author_browse" 1 "-Dbibleech=StoredFieldLeech -Dsortfield=author:author2 -Dvaluefield=author:author2 -Dfilterfield=building:source_str_mv"
+#build_browse "lcc" "callnumber-a" 1 "-Dsortfield=callnumber-a -Dvaluefield=callnumber-a -Dbuildfield=building:source_str_mv"
+#build_browse "dewey" "dewey-raw" 1 "-Dbibleech=StoredFieldLeech -Dsortfield=dewey-sort -Dvaluefield=dewey-raw -Dbuildfield=building:source_str_mv"
